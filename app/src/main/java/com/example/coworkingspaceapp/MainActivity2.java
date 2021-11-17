fceb4efd50e45d47dba34aaec79ced22cb5c63f1
skander-ayedi <68228202+skander-ayedi@users.noreply.github.com>
2021-11-17 16:54:34 +0100

    new

--- app/src/main/java/com/example/coworkingspaceapp/MainActivity2.java
+++ app/src/main/java/com/example/coworkingspaceapp/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.coworkingspaceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -9,6 +10,6 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+
     }
 }
